f8cea314b41e0dd8ca4eee261c9948f6eb73f65b
BubbleSci <1228160604@qq.com>
2020-06-24 14:58:41 +0800

    text

---
+++ pages/index/index.js
@@ -0,0 +1,15 @@
+//index.js
+//获取应用实例
+//const app = getApp()
+Page({
+  getNum:function(){//这里的获取值和html不一样
+			// let appleNum = document.getElementById("aNum").value;
+			// let bananaNum = document.getElementById("bNum").value;
+      // let dragonNum = document.getElementById("dNum").value;
+      // let aprise = aNum*1;
+      // let bprise = bNum*5;
+      // let dprise = dNum*10;
+      // let total = aprise+bprise+dprise;
+      // console.log( total),
+  }
+      })
---
+++ pages/logs/logs.js
@@ -0,0 +1,21 @@
+//logs.js
+//const util = require('../../utils/util.js')
+
+Page({
+  data: {
+    logs: [{
+        name:'苹果',age:'aprise',
+        name:'香蕉',age:'bprise',
+        name:'火龙果',age:'dprise',
+        name:'西瓜',age:'xprise',
+      }
+    ]
+  },
+  // onLoad: function () {
+  //   this.setData({
+  //     logs: (wx.getStorageSync('logs') || []).map(log => {
+  //       return util.formatTime(new Date(log))
+  //     })
+  //   })
+  // }
+})
