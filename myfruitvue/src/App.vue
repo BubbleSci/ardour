f3f9f8d55a5d28e81bab2beb72519d266a4a1261
BubbleSci <1228160604@qq.com>
2020-07-01 15:50:53 +0800

    vue

---
+++ myfruitvue/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">水果大甩卖</router-link> |
+      <router-link to="/pay">付款</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+  #nav{
+    width: 100%;
+    line-height: 50px;
+    text-align: center;
+  }
+</style>
---
+++ myfruitvue/src/views/Pay.vue
@@ -0,0 +1,42 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+export default {
+	methods: {
+		data() {
+			return{}
+		},
+		getRequest(urlStr) {
+			if (typeof urlStr == "undefined") {
+				var url = decodeURI(location.search);
+			} else {
+				var url = "?" + urlStr.split("?")[1];
+			}
+			var theRequest = new Object();
+			if (url.indexOf("?") != -1) {
+				var str = url.substr(1);
+				var strs = str.split("&");
+				for (var i = 0; i < strs.length; i++) {
+					theRequest[strs[i].split("=")[0]] = decodeURI(strs[i].split("=")[1]);
+				}
+			}
+			return theRequest;
+		}
+	},
+	mounted() {
+		let arr = this.getRequest(location.search);
+		let prise = arr["total"];
+		let apple = arr["apple"];
+		let banana = arr["banana"];
+		let dragon = arr["dragon"];
+		alert(
+			"苹果" + apple + "香蕉" + banana + "火龙果" + dragon + "总计" + prise
+		);
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
